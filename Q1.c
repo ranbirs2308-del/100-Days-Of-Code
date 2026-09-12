f3acb130eda4b6ad55395d4bd1b5fde1eb459aea
Ranbir <ranbirs2308@gmail.com>
2026-09-12 22:36:06 +0530

    Add Q1 solution

---
+++ Q1.c
@@ -0,0 +1,14 @@
+// Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the first number- ");
+    scanf("%d", &a);
+    int b;
+    printf("Enter the second number- ");
+    scanf("%d", &b);
+    int c = a + b;
+    printf("Sum of your 2 digits is %d", c);
+    return 0;
+}
