e85dabe7293b986e5bee127e9e18c5531b1b8039
Ranbir <ranbirs2308@gmail.com>
2026-09-13 12:21:13 +0530

    Solution for Q7

---
+++ Q7.c
@@ -0,0 +1,11 @@
+// Write a program to swap two numbers without using a third variable.
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("Enter num1: ");
+    scanf("%d", &a);
+    printf("Enter num2: ");
+    scanf("%d", &b);
+    printf("Here num1 = %d and num2 = %d", b, a);
+    return 0;
+}
