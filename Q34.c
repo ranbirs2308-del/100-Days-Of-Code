ef548685cabbce04b75167e513b526fba009b8f0
Ranbir <ranbirs2308@gmail.com>
2026-09-18 18:17:57 +0530

    Solution for Q34

---
+++ Q34.c
@@ -0,0 +1,28 @@
+// Write a program to check if a number is prime.
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if (count == 2)
+    {
+        printf("Prime Number");
+    }
+    else
+    {
+        printf("Not Prime Number");
+    }
+
+    return 0;
+}
