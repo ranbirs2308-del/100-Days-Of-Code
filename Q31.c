ae46dac0efee555b4c6ed4f1fb143ce64857470e
Ranbir <ranbirs2308@gmail.com>
2026-09-18 18:12:31 +0530

    Solution for Q31

---
+++ Q31.c
@@ -0,0 +1,21 @@
+// Write a program to take a number as input and print its equivalent binary representation.
+#include <stdio.h>
+
+int main()
+{
+    int n, remainder, binary = 0, place = 1;
+
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        remainder = n % 2;
+        binary = binary + remainder * place;
+        place = place * 10;
+        n = n / 2;
+    }
+
+    printf("%d", binary);
+
+    return 0;
+}
