1c21ccbad8705e2d88886b998c65a089dfc45780
Ranbir <ranbirs2308@gmail.com>
2026-09-19 16:05:21 +0530

    Solution for Q40

---
+++ Q40.c
@@ -0,0 +1,32 @@
+// Write a program to find the 1’s complement of a binary number and print it.
+#include <stdio.h>
+
+int main()
+{
+    int n, digit, complement = 0, place = 1;
+
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+
+        if (digit == 0)
+        {
+            digit = 1;
+        }
+        else
+        {
+            digit = 0;
+        }
+
+        complement = complement + digit * place;
+        place = place * 10;
+
+        n = n / 10;
+    }
+
+    printf("%d", complement);
+
+    return 0;
+}
