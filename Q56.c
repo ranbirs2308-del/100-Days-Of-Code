689342833a64d0ae3ed7e91c801c2ea3d8967ac2
Ranbir <ranbirs2308@gmail.com>
2026-09-22 19:25:04 +0530

    Solution for Q56

---
+++ Q56.c
@@ -0,0 +1,21 @@
+// Read and print elements of a one-dimensional array.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
