aacbef2189a76a94f0e9b2324be548b23553fc5f
Ranbir <ranbirs2308@gmail.com>
2026-09-22 19:26:27 +0530

    Solution for Q57

---
+++ Q57.c
@@ -0,0 +1,23 @@
+// Find the sum of array elements.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
